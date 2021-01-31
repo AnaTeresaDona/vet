b2ccde014167de39ac574a35a9677bdaf9ab01db
AnaTeresaDona <anatdona@gmail.com>
2021-01-31 15:34:11 -0300

    veterinario fk created, methods in pet model. Alteration of MVC to adjust to the expected params

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,22 +1,32 @@
 class Pet < ApplicationRecord
+  has_many :pet_histories, dependent: :destroy
+
+  def to_s 
+    name
+  end
 
   def history_count
+    pet_histories.count
     #TODO-implement
   end
 
   def avg_weight
+    pet_histories.average :weight
     #TODO-implement
   end
 
   def avg_height
+    pet_histories.average :heigth
     #TODO-implement
   end
 
   def max_weight
+    pet_histories.pluck(:weight).max
     #TODO-implement
   end
 
   def max_height
+    pet_histories.pluck(:heigth).max
     #TODO-implement
   end
 
